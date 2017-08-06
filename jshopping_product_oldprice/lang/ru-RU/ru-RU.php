a94838f360f55fd9fc30e32f0d75a5069765324f
Igor Levoshkin <30287101+levoshkin@users.noreply.github.com>
2017-08-06 13:52:17 +0300

    Add files via upload

---
+++ jshopping_product_oldprice/lang/ru-RU/ru-RU.php
@@ -0,0 +1,16 @@
+<?php
+defined('_JEXEC') or die();
+
+define('_JSHOP_CLOSE',"Закрыть");
+define('_JSHOP_EDIT_PRICE',"Редактировать цены");
+define('_JSHOP_SAVE_MODIFICATION',"Сохранить изменения");
+define('_JSHOP_SELECT_CURRENCY',"Выберите валюту");
+define('_JSHOP_SELECT',"Выберите");
+define('_JSHOP_PRICE',"Цена");
+define('_JSHOP_OLD_PRICE',"Старая цена");
+define('_JSHOP_QUANTITY',"Количество");
+define('_JSHOP_EAN_PRODUCT',"Код товара");
+define('_JSHOP_START_DATE',"Дата начала");
+define('_JSHOP_EXPIRE_DATE',"Дата окончания");
+define('_JSHOP_APPLY',"Применить");
+define('_JSHOP_ERROR_PRODUCT_NOT_EXIST', 'Ошибка. Продукт не существует.<br>Сначала создайте продукт.');
