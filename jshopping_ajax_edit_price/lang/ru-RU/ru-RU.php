ec1eaa94afc4dba070f54b4759967f8dd4698335
Igor Levoshkin <30287101+levoshkin@users.noreply.github.com>
2017-08-06 13:26:50 +0300

    Add files via upload

---
+++ jshopping_ajax_edit_price/lang/ru-RU/ru-RU.php
@@ -0,0 +1,12 @@
+<?php
+defined('_JEXEC') or die();
+
+define('_JSHOP_CLOSE',"Закрыть");
+define('_JSHOP_EDIT_PRICE',"Редактировать цены");
+define('_JSHOP_SAVE_MODIFICATION',"Сохранить изменения");
+define('_JSHOP_SELECT_CURRENCY',"Выберите валюту");
+define('_JSHOP_SELECT',"Выберите");
+define('_JSHOP_PRICE',"Цена");
+define('_JSHOP_OLD_PRICE',"Старая цена");
+define('_JSHOP_QUANTITY',"Количество");
+define('_JSHOP_EAN_PRODUCT',"Код товара");
